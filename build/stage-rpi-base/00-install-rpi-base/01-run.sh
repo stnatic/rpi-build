2d6b3cda26ff7374b446c59d1629c751a072c648
Krzysztof Kulig <kris@tango.agency>
2025-05-30 00:59:11 +0200

    Add build steps

---
+++ build/stage-rpi-base/00-install-rpi-base/01-run.sh
@@ -0,0 +1,26 @@
+function info() {
+  [[ "$CI" = true ]] && echo "::group::$*" || >&2 echo "$*"
+}
+
+function endinfo() {
+  [[ "$CI" = true ]] && echo "::endgroup::" || true
+}
+
+info "updating the login screen and message of the day"
+export VERSION
+rm ${ROOTFS_DIR}/etc/motd
+rm ${ROOTFS_DIR}/etc/update-motd.d/10-uname
+envsubst < files/etc/update-motd.d/10-hello.template > ${ROOTFS_DIR}/etc/update-motd.d/10-hello
+chmod a+x ${ROOTFS_DIR}/etc/update-motd.d/10-hello
+
+rm ${ROOTFS_DIR}/etc/issue
+envsubst < files/etc/issue.template > ${ROOTFS_DIR}/etc/issue
+endinfo
+
+info "expire default password to force change on first login"
+on_chroot << EOF
+  chage --lastday 0 pi
+EOF
+
+install files/home/pi/disable-ssh-password-auth "${ROOTFS_DIR}/home/pi/"
+endinfo
